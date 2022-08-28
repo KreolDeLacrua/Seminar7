6f7dc025742563b01a95c6bd05092f07e2d023b6
alesya <kreol_11@inbox.ru>
2022-08-28 13:31:38 +0300

    task48

---
+++ task48/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5
+*/
+
+int[,] FillMatrix(int rowCount, int columnsCount)
+{
+    int[,] matrix = new int[rowCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите m и n через Enter "); 
+int m = Convert.ToInt32(Console.ReadLine()); //чисто строк
+int n = Convert.ToInt32(Console.ReadLine()); //число столбцов
+
+int[,] matrix = FillMatrix(m, n);
+PrintMatrix(matrix);
