50b6dbc0e31c69f1f66817d5df8e66ec15cb9055
alesya <kreol_11@inbox.ru>
2022-08-28 13:32:21 +0300

    task50

---
+++ task50/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Задача 50. Напишите программу, 
+которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1 7 -> такого числа в массиве нет
+*/
+
+int[,] FillMatrix(int rowCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = FillMatrix(3, 5);
+
+PrintMatrix(matrix);
+
+Console.Write("Введите координаты позиции элемента, разделенных запятой (нумерация с единицы): ");
+Console.Write("Введите номер строки: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(matrix[m - 1, n - 1]);
+
+
+
+
